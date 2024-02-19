75e000433402d76da6ea889e9dd246e4c9afa57f
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-02-19 13:43:53 -0300

    calculadora finalizada

--- calculadora.js
+++ calculadora.js
@@ -4,17 +4,32 @@ const teclaRes = document.querySelector(".res")
 const Display = document.querySelector(".display")
 const teclaOn = document.querySelector("#teclaoff")
 const teclaLimpar = document.querySelector("#limpar")
+const teclaIgual = document.querySelector("#resultado")
 
 let sinal = false
+let decimal = false
 
 teclasNum.map((upu)=>{
     upu.addEventListener("click",(evt)=>{
         sinal = false
-        if(Display.innerHTML == "0"){
-            Display.innerHTML = ""
+    if(evt.target.innerHTML == ","){
+            if(!decimal){
+                decimal = true
+                if(decimal.innerHTML == "0"){
+                    decimal.innerHTML = "0,"
+                }else{
+                    Display.innerHTML += evt.target.innerHTML  
+                }
+            }
+        }else{
+            if(Display.innerHTML == "0"){
+                Display.innerHTML = ""
+            }
+            
+            Display.innerHTML += evt.target.innerHTML  
         }
-        Display.innerHTML += evt.target.innerHTML
-   })
+       
+    })
 })
 teclasOp.map((upu)=>{
     upu.addEventListener("click",(evt)=>{
@@ -22,19 +37,37 @@ teclasOp.map((upu)=>{
             sinal = true
             if(Display.innerHTML == "0"){
                 Display.innerHTML = ""
-
             }
-            if(evt.target.innerHTML == "X"){
-                Display.innerHTML += "*"
-            }else{
-                Display.innerHTML += evt.target.innerHTML  
-            }
+         if(evt.target.innerHTML == "X"){
+            Display.innerHTML += "*"
+         }else{
+             Display.innerHTML += evt.target.innerHTML
+         }
         }
-        
     })
-        
 })
 teclaOn.addEventListener("click",(evt)=>{
-        Display.innerHTML = 0
-})
+    sinal = false
+    decimal = false
 
+    Display.innerHTML = "0"
+    
+})
+teclaIgual.addEventListener("click",(evt)=>{
+    sinal = false
+    decimal = false
+    const res = eval(Display.innerHTML)
+    Display.innerHTML = res
+})
+// teclaLimpar.addEventListener("click",(evt)=>{
+//     let displayConteudo = Display.innerHTML
+//     if(displayConteudo.length > 0){
+//        Display.innerHTML = displayConteudo.slice(0, -1)
+       
+//     }
+// })
+teclaLimpar.addEventListener("click",(evt)=>{
+    if(Display.innerHTML.length > 0){
+     Display.innerHTML  = Display.innerHTML.slice(0, -1)
+    }
+    })
